5dc92f06e2403c4a6351d49699c6618a6f49bc90
Jonas Barth <143815039+barthjs@users.noreply.github.com>
2025-04-15 21:55:39 +0200

    Add Composer and NVM

--- shell/exports.sh
+++ shell/exports.sh
@@ -12,3 +12,11 @@ export FZF_CTRL_T_OPTS="
   --walker-skip .git,node_modules,target
   --preview 'bat -n --color=always {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+# Composer bin
+export PATH=$HOME/.config/composer/vendor/bin/:$PATH
+
+# Node Version Manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
